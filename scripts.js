3754a94a6a5bffa3247d7e2baafb1828d8c7c842
filipdavidovic <filip.davidovic.fd@gmail.com>
2017-05-30 19:08:18 +0200

    dashboard update, interval of measurement for various apps

--- scripts.js
+++ scripts.js
@@ -12,14 +12,46 @@ $(document).ready(function() {
     Cookies.set("units", {
       "temperatureUnit": "c", // Celsius
       "bloodPressureUnit": "eu", // kPa
-      "distanceUnit": "km" // KiloMeters
+      "distanceUnit": "km" // Kilometers
     }, { expires: 7 });
     Cookies.set("dyslexic", false, { expires: 7 });
     Cookies.set("highContrast", false, { expires: 7 });
     Cookies.set("shortcuts", false);
-    console.log("registered cookie");
+
+    Cookies.set("landingTiles", {
+      "hRate": true,
+      "bPressure": true,
+      "aTemp": true,
+      "bTemp": true,
+      "stress": true,
+      "sleep": true
+    }, { expires: 7 });
+
+    localStorage.setItem("hr", getRandom(50, 190));
+    localStorage.setItem("bp", getRandom(120, 180));
+    localStorage.setItem("hrBpInterval", 30);
+
+    localStorage.setItem("lastInterval", setInterval(function() {
+      localStorage.setItem("hr", getRandom(50, 190));
+      localStorage.setItem("bp", getRandom(120, 180));
+    }, localStorage.getItem("hrBpInterval") * 60 * 1000));
+
+    localStorage.setItem("aTemp", getRandom(-10, 40));
+    localStorage.setItem("bTemp", getRandom(32, 40));
+    localStorage.setItem("tempInterval", 30);
+
+    localStorage.setItem("lastTempInterval", setInterval(function() {
+      localStorage.setItem("aTemp", getRandom(-10, 40));
+      localStorage.setItem("bTemp", getRandom(32, 40));
+    }, localStorage.getItem("tempInterval") * 60 * 1000));
   } else {
-    console.log("we have a cookie");
+    if(localStorage.getItem("hrBpInterval") === null) {
+      localStorage.setItem("hrBpInterval", 30);
+    }
+
+    if(localStorage.getItem("tempInterval") === null) {
+      localStorage.setItem("tempInterval", 30);
+    }
   }
 
   $(document).keypress(function(e) {
@@ -55,13 +87,29 @@ $(document).ready(function() {
   });
 
   if(Cookies.get("highContrast") === "true") {
-    $( "<style id=\"highContrastStyle\">body { background-color: black; } a { color: #885555; } .topbar { color: black; } .notification { border-left-color: #442a2a; background-color: #885555; color: black; }</style>" ).appendTo( "head" );
+    $( "<style id=\"highContrastStyle\">body { background-color: black; } .green { color: #885555; } a { color: #885555; } .topbar { color: black; } .notification { border-left-color: #442a2a; background-color: #885555; color: black; }</style>" ).appendTo( "head" );
   }
 
   if(Cookies.get("dyslexic") === "true") {
     $( "<style id=\"dyslexicStyle\">p, a, h2 { font-family: 'opendyslexic'; }</style>" ).appendTo( "head" );
   }
+
+  updateHrBp();
+  clearInterval(localStorage.getItem("lastInterval"));
+  localStorage.setItem("lastInterval", setInterval(function() {
+    updateHrBp();
+  }, localStorage.getItem("hrBpInterval") * 60 * 1000));
+
+  updateTemp();
+  clearInterval(localStorage.getItem("lastTempInterval"));
+  localStorage.setItem("lastTempInterval", setInterval(function() {
+    updateTemp();
+  }, localStorage.getItem("tempInterval") * 60 * 1000));
 });
+
+function getRandom(min, max) {
+    return Math.random() * (max - min) + min;
+}
 
 var tempConversion = (function() {
   var CtoF = function(degrees) {
@@ -123,7 +171,6 @@ function callPage(pageRefInput) {
     //   $("<div class=\"conetent\">").html(json.html).appendTo("body");
     // }
     success: function(response) {
-      //console.log("page loaded successfully: ", response);
       $('.innerContent').html(response);
 
 	  var containers=$('.scaleContainer');
@@ -191,7 +238,7 @@ function callPage(pageRefInput) {
       $("#toggleHighContrast").click(function() {
         if(Cookies.get("highContrast") === "false") {
           Cookies.set("highContrast", true);
-          $( "<style id=\"highContrastStyle\">body { background-color: black; } a { color: #885555; } .topbar { color: black; } .notification { border-left-color: #442a2a; background-color: #885555; color: black; }</style>" ).appendTo( "head" );
+          $( "<style id=\"highContrastStyle\">body { background-color: black; } .green { color: #885555; } a { color: #885555; } .topbar { color: black; } .notification { border-left-color: #442a2a; background-color: #885555; color: black; }</style>" ).appendTo( "head" );
         } else {
           Cookies.set("highContrast", false);
           $("#highContrastStyle").remove();
@@ -199,9 +246,9 @@ function callPage(pageRefInput) {
       });
 
       if(Cookies.get("shortcuts") === "true") {
-        $("#shortcutsCheckbox").prop("checked", "true");
+        $("#shortcutsCheckbox").prop("checked", true);
       } else {
-        $("#shortcutsCheckbox").prop("checked", "false");
+        $("#shortcutsCheckbox").prop("checked", false);
       }
       $("#shortcutsCheckbox").change(function() {
         if($("#shortcutsCheckbox").prop("checked") === false) {
@@ -211,28 +258,213 @@ function callPage(pageRefInput) {
         }
       });
     } else if(pageRefInput === "heart-rate.html") {
+      doUpdateHrBp();
+
+      $("#hrBpIntervalOutput").text(localStorage.getItem("hrBpInterval"));
+
       var tempCookie = Cookies.getJSON("units");
-      if($("#bpUnit").text() === "mmHg") {
-        if(tempCookie.bloodPressureUnit === "eu") {
-          $("#bpValue").text( round(bpConversion.usToEu( $("#bpValue").text()), 1) );
-          $("#bpUnit").text("kPa");
-        }
+
+      if(tempCookie.bloodPressureUnit === "eu") {
+        $("#bpOutput").text( round(bpConversion.usToEu( localStorage.getItem("bp")), 1) );
+        $("#bpUnit").text("kPa");
+      } else {
+        $("#bpOutput").text(localStorage.getItem("bp"));
+        $("#bpUnit").text("mmHg");
       }
+
+      $( "#hrBpSlider" ).slider({
+        min: 0.1,
+        max: 100,
+        orientation: "horizontal",
+        step: 1,
+        value: localStorage.getItem("hrBpInterval"),
+        slide: function( event, ui ) {
+                  localStorage.setItem("hrBpInterval", ui.value);
+                  $("#hrBpIntervalOutput").text(ui.value);
+                  clearInterval(localStorage.getItem("lastInterval"));
+                  localStorage.setItem("lastInterval", setInterval(function() {
+                    updateHrBp();
+                  }, localStorage.getItem("hrBpInterval") * 60 * 1000));
+               }
+      });
+
+      var landingJSON = Cookies.getJSON("landingTiles");
+      if(landingJSON.hRate === true) {
+        $("#hRateCheckbox").prop("checked", true);
+      } else {
+        $("#hRateCheckbox").prop("checked", false);
+      }
+      $("#hRateCheckbox").change(function() {
+        if($("#hRateCheckbox").prop("checked") === false) {
+          Cookies.set("landingTiles", {
+            "hRate": false,
+            "bPressure": landingJSON.bPressure,
+            "aTemp": landingJSON.aTemp,
+            "bTemp": landingJSON.bTemp,
+            "stress": landingJSON.stress,
+            "sleep": landingJSON.sleep
+          });
+        } else {
+          Cookies.set("landingTiles", {
+            "hRate": true,
+            "bPressure": landingJSON.bPressure,
+            "aTemp": landingJSON.aTemp,
+            "bTemp": landingJSON.bTemp,
+            "stress": landingJSON.stress,
+            "sleep": landingJSON.sleep
+          });
+        }
+
+        console.log(Cookies.getJSON("landingTiles"));
+      });
+
+      landingJSON = Cookies.getJSON("landingTiles");
+      if(landingJSON.bPressure === true) {
+        $("#bPressureCheckbox").prop("checked", true);
+      } else {
+        $("#bPressureCheckbox").prop("checked", false);
+      }
+      $("#bPressureCheckbox").change(function() {
+        if($("#bPressureCheckbox").prop("checked") === false) {
+          Cookies.set("landingTiles", {
+            "hRate": landingJSON.hRate,
+            "bPressure": false,
+            "aTemp": landingJSON.aTemp,
+            "bTemp": landingJSON.bTemp,
+            "stress": landingJSON.stress,
+            "sleep": landingJSON.sleep
+          });
+        } else {
+          Cookies.set("landingTiles", {
+            "hRate": landingJSON.hRate,
+            "bPressure": true,
+            "aTemp": landingJSON.aTemp,
+            "bTemp": landingJSON.bTemp,
+            "stress": landingJSON.stress,
+            "sleep": landingJSON.sleep
+          });
+        }
+      });
     } else if(pageRefInput === "temperature.html") {
+      doUpdateTemp();
+
+      $("#tempIntervalOutput").text(localStorage.getItem("tempInterval"));
+
       var tempCookie = Cookies.getJSON("units");
       if(tempCookie.temperatureUnit === "f") {
-        $("#ambientTempValue").text( round(tempConversion.CtoF( $("#ambientTempValue").text()), 1) );
+        $("#ambientTempValue").text( round(tempConversion.CtoF( localStorage.getItem("aTemp")), 1) );
         $("#ambientTempUnit").html("&#8457;");
 
-        $("#bodyTempValue").text( round(tempConversion.CtoF( $("#bodyTempValue").text()), 1) );
+        $("#bodyTempValue").text( round(tempConversion.CtoF( localStorage.getItem("bTemp")), 1) );
         $("#bodyTempUnit").html("&#8457;");
       }
+
+      $( "#tempSlider" ).slider({
+        min: 0.1,
+        max: 100,
+        orientation: "horizontal",
+        step: 1,
+        value: localStorage.getItem("tempInterval"),
+        slide: function( event, ui ) {
+                  localStorage.setItem("tempInterval", ui.value);
+                  $("#tempIntervalOutput").text(ui.value);
+                  clearInterval(localStorage.getItem("lastTempInterval"));
+                  localStorage.setItem("lastTempInterval", setInterval(function() {
+                    updateTemp();
+                  }, localStorage.getItem("tempInterval") * 60 * 1000));
+               }
+      });
+
+      var landingJSON = Cookies.getJSON("landingTiles");
+      if(landingJSON.aTemp === true) {
+        $("#aTempCheckbox").prop("checked", true);
+      } else {
+        $("#aTempCheckbox").prop("checked", false);
+      }
+      $("#aTempCheckbox").change(function() {
+        if($("#aTempCheckbox").prop("checked") === false) {
+          Cookies.set("landingTiles", {
+            "hRate": landingJSON.hRate,
+            "bPressure": landingJSON.bPressure,
+            "aTemp": false,
+            "bTemp": landingJSON.bTemp,
+            "stress": landingJSON.stress,
+            "sleep": landingJSON.sleep
+          });
+        } else {
+          Cookies.set("landingTiles", {
+            "hRate": landingJSON.hRate,
+            "bPressure": landingJSON.bPressure,
+            "aTemp": true,
+            "bTemp": landingJSON.bTemp,
+            "stress": landingJSON.stress,
+            "sleep": landingJSON.sleep
+          });
+        }
+      });
+
+      landingJSON = Cookies.getJSON("landingTiles");
+      if(landingJSON.aTemp === true) {
+        $("#bTempCheckbox").prop("checked", true);
+      } else {
+        $("#bTempCheckbox").prop("checked", false);
+      }
+      $("#bTempCheckbox").change(function() {
+        if($("#bTempCheckbox").prop("checked") === false) {
+          Cookies.set("landingTiles", {
+            "hRate": landingJSON.hRate,
+            "bPressure": landingJSON.bPressure,
+            "aTemp": landingJSON.bTemp,
+            "bTemp": false,
+            "stress": landingJSON.stress,
+            "sleep": landingJSON.sleep
+          });
+        } else {
+          Cookies.set("landingTiles", {
+            "hRate": landingJSON.hRate,
+            "bPressure": landingJSON.bPressure,
+            "aTemp": landingJSON.aTemp,
+            "bTemp": true,
+            "stress": landingJSON.stress,
+            "sleep": landingJSON.sleep
+          });
+        }
+      });
     } else if(pageRefInput === "landing.html") {
       $(".ajaxLink").on('click', function(e){
         e.preventDefault();
         var pageRef = $(this).attr('href');
         callPage(pageRef);
       });
+
+      var landingJSON = Cookies.getJSON("landingTiles");
+      if(landingJSON.hRate) {
+        $("#hrTile .value").text(localStorage.getItem("hr") + " BPM");
+        $("#hrCol").css("display", "inline");
+      } else {
+        $("#hrCol").css("display", "none");
+      }
+
+      var tempJSON = Cookies.getJSON("units");
+      if(landingJSON.bPressure) {
+        $("#bpTile .value").text(tempJSON.bloodPressureUnit === "eu" ? round(bpConversion.usToEu(localStorage.getItem("bp")), 1) + " kPa" : localStorage.getItem("bp") + " mmHg");
+        $("#bpCol").css("display", "inline");
+      } else {
+        $("#bpCol").css("display", "none");
+      }
+
+      if(landingJSON.aTemp) {
+        $("#ambientTempTile .value").text(tempJSON.temperatureUnit === "f" ? round(tempConversion.CToF(localStorage.getItem("aTemp")), 1) + " F" : localStorage.getItem("aTemp") + " C"); //  TODO: add celsius and farenheight hex
+        $("#aTempCol").css("display", "inline");
+      } else {
+        $("#aTempCol").css("display", "none");
+      }
+      if(landingJSON.aTemp) {
+        $("#bodyTempTile .value").text(tempJSON.temperatureUnit === "f" ? round(tempConversion.CToF(localStorage.getItem("bTemp")), 1) + " F" : localStorage.getItem("bTemp") + " C"); //  TODO: add celsius and farenheight hex
+        $("#bTempCol").css("display", "inline");
+      } else {
+        $("#bTempCol").css("display", "none");
+      }
     }
 
     },
@@ -258,3 +490,53 @@ function callPage(pageRefInput) {
   }, 1000);
 
 }());
+
+var updateHrBp = function() {
+  localStorage.setItem("hr", round(getRandom(50, 190), 1));
+
+  localStorage.setItem("bp", round(getRandom(120, 180), 1));
+
+  doUpdateHrBp();
+};
+
+var doUpdateHrBp = function() {
+  var tempCookie = Cookies.getJSON("units");
+
+  $("#hrOutput").text(localStorage.getItem("hr"));
+
+  if(tempCookie.bloodPressureUnit === "eu") {
+    $("#bpOutput").text( round(bpConversion.usToEu(localStorage.getItem("bp")), 1) );
+    $("#bpUnit").text("kPa");
+  } else {
+    $("#bpOutput").text(localStorage.getItem("bp"));
+    $("#bpUnit").text("mmHg");
+  }
+}
+
+var updateTemp = function() {
+  localStorage.setItem("aTemp", round(getRandom(-10, 40), 1));
+
+  localStorage.setItem("bTemp", round(getRandom(32, 40), 1));
+
+  doUpdateTemp();
+}
+
+var doUpdateTemp = function() {
+  var tempCookie = Cookies.getJSON("units");
+
+  if(tempCookie.bloodPressureUnit === "f") {
+    $("#ambientTempValue").text( round(tempConversion.CToF(localStorage.getItem("bTemp")), 1) );
+    $("#ambientTempUnit").text("F"); // TODO: add farenhight hex
+  } else {
+    $("#ambientTempValue").text(localStorage.getItem("aTemp"));
+    $("#ambientTempUnit").text("C"); // TODO: add celsius hex
+  }
+
+  if(tempCookie.bloodPressureUnit === "f") {
+    $("#bodyTempValue").text( round(tempConversion.CToF(localStorage.getItem("bTemp")), 1) );
+    $("#bodyTempUnit").text("F"); // TODO: add farenhight hex
+  } else {
+    $("#bodyTempValue").text(localStorage.getItem("bTemp"));
+    $("#bodyTempUnit").text("C"); // TODO: add celsius hex
+  }
+}
